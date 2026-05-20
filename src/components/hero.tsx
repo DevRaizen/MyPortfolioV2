0c3343e565eadb2b511cac0ff7d6943f4981fb42
DevRaizen <shawnbulos03@gmail.com>
2026-05-20 13:21:03 +0800

    add link

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,11 @@
+import profile from "../assets/profile.jpg";
+
 function Hero() {
   return (
     <div className="border border-gray-300 shadow-lg p-4 rounded-lg">
       <div className="flex flex-row items-start gap-4">
         <div className="w-32 h-32 rounded-full overflow-hidden flex-shrink-0">
-          <img src="/src/assets/profile.jpg" alt="" className=" object-cover" />
+          <img src={profile} alt="Profile" className="object-cover" />
         </div>
         <div className="p-2">
           <h1 className="text-3xl font-bold text-gray-800 mt-4">
@@ -13,11 +15,10 @@ function Hero() {
             Junior Web Developer
           </h2>
           <span className="text-gray-600">
-            Passionate about building Web
-            Applications. I also explore AI tools and MCP servers to improve
-            development efficiency and automate workflows. Focused on writing
-            clean code, continuous learning, and delivering efficient,
-            high-quality solutions.
+            Passionate about building Web Applications. I also explore AI tools
+            and MCP servers to improve development efficiency and automate
+            workflows. Focused on writing clean code, continuous learning, and
+            delivering efficient, high-quality solutions.
           </span>
         </div>
       </div>
